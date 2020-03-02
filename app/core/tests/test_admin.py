497622f29c22021d553f9ecf14743f2d73696aaa
nabenabe0258 <nabenabe0258@gmail.com>
2020-03-02 20:24:21 +0900

    updata admin to support custom user admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(email="test@gmail.com", password="adminadmin")
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(email="test1@gmail.com",password="useruser")
+
+
+    def test_user_listed(self):
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+        self.assertContains(res,self.user.name)
+        self.assertContains(res,self.user.email)
+
+
+    def test_user_change_page(self):
+        url = reverse('admin:core_user_change',args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code,200)
+
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code,200)
